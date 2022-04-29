1a99e484862bd10ae9baaa8feb311cb6cd1e64d4
Krzysiek <krzysztofkulpacb@gmail.com>
2022-04-29 23:12:07 +0200

    0.0.1 added: getNodeByRowColumnIndex, moveNodeToCoordinates

--- src/main/java/com/kkulpa/checkers/checkers/BoardController.java
+++ src/main/java/com/kkulpa/checkers/checkers/BoardController.java
@@ -15,31 +15,58 @@ public class BoardController {
     @FXML
     private GridPane board;
 
-    @FXML
-    private ImageView redPawn1;
 
 
     public void onPawnClick(MouseEvent e){
+        System.out.println(" ");
 
-        ObservableList<Node> childrens = board.getChildren();
-        Node clickedNode = null;
-        if(e.getTarget() instanceof Node){
-            clickedNode = (Node)e.getTarget();
-        }
+        Node clickedNode = (Node)e.getTarget();
 
-//        for(Node node : childrens){
-//            if(node.equals(clickedNode)){
-//
-//            }
-//
-//
-//        }
+        Integer c = GridPane.getColumnIndex(clickedNode);
+        Integer r = GridPane.getRowIndex(clickedNode);
 
-
+        int targerColumnIndex  = c == null ? 0 : c;
+        int targetRowIndex     = r == null ? 0 : r;
 
         System.out.println("clicked " + clickedNode.getId());
+        System.out.println("Adress: " + targerColumnIndex + " " + targetRowIndex);
 
 
+
+
+
+
+
+
+
+    }
+
+
+
+    private Node getNodeByRowColumnIndex ( final int column, final int row, GridPane gridPane) {
+        Node result = null;
+        ObservableList<Node> childrens = gridPane.getChildren();
+
+        for (Node node : childrens) {
+            Integer c = GridPane.getColumnIndex(node);
+            Integer r = GridPane.getRowIndex(node);
+            int currentNodeColumnIndex  = c == null ? 0 : c;
+            int currentNodeRowIndex     = r == null ? 0 : r;
+
+            if(currentNodeRowIndex == row && currentNodeColumnIndex == column) {
+                result = node;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private void moveNodeToCoordinates(Node node, GridPane board, int column, int row){
+        if (getNodeByRowColumnIndex(column, row, board) == null ) {
+            GridPane.setColumnIndex(node, column);
+            GridPane.setRowIndex(node,row);
+        }
 
     }
 
